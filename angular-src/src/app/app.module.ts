e06735293da619f422f4138f261928730eaab1e2
enavuio <enavuio@MacBooks-MacBook-Pro.local>
2017-07-13 00:14:37 -0600

    register page

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+
+import {ValidateService} from './services/validate.service';
 const appRoutes: Routes = [
   {path:'', component: HomeComponent},
   {path:'register', component: RegisterComponent},
@@ -34,7 +36,7 @@ const appRoutes: Routes = [
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
